51f00776bcba96ca1a9fc188a8850b9f9744f1c0
rana_satou <rana.satou@gmail.com>
2019-04-06 17:28:38 +0900

    lesson7 課題提出。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // "AudioSource"コンポーネントを格納する変数
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start() {
+        // "AudioSource"コンポーネントを変数"audioSource"に格納する
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine) {
+            Destroy(gameObject);
+        }
+    }
+
+    // キューブが地面に接触する時と積み重なるときに効果音
+    void OnCollisionEnter2D(Collision2D collision) {
+       
+            GetComponent<AudioSource>().Play();
+            //Debug.Log(audioSource);
+
+    }
+}
